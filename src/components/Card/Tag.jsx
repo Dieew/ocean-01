672cf6ba3ab459ba5b822362e534728aebd77c51
Dieew <79275765+Dieew@users.noreply.github.com>
2024-01-30 12:58:01 +0000

    tags estilização

--- src/components/Card/Tag.jsx
+++ src/components/Card/Tag.jsx
@@ -1,6 +1,6 @@
 function Tag() {
     return (
-        <div>
+        <div className="tags">
             <h3 className="status">Status: Vivo</h3>
                 <h3 className="status">Espécie: Humana</h3>
                     <h3 className="status">Origem: Terra C-137</h3>
