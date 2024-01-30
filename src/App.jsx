5316847ddc21ffec3b274c64f778ac085c00e2d5
Dieew <79275765+Dieew@users.noreply.github.com>
2024-01-30 13:05:10 +0000

    const item1 - rick

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Card from './components/Card/Card'
 
 
 function App() {
+  const item1 ={
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
   return (
     <>
     <Card/>
