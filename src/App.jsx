16f9708d7e13711673a69cdd5c5f47eea01450be
Dieew <79275765+Dieew@users.noreply.github.com>
2024-01-31 14:38:50 +0000

    Criação do estado de itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 
 function App() {
   //carregamento de dados via código//
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-  const item4 = {
-    name: 'Beth Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  }
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  // }
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
+  // const item4 = {
+  //   name: 'Beth Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  // }
 
-  const itens = [item1,item2,item3,item4]
-  //carregamento de dados via API backend\\
+  // const itens = [item1,item2,item3,item4]
+
+
+
+  const [itens, setItens] = useState ([])
+
+
   async function carregarDadosApi() {
   //Declarar a URL da API
     const apiUrl ='https://rickandmortyapi.com/api/character/'
@@ -35,9 +41,14 @@ function App() {
 
   const body = await response.json()
   
-  console.log(body)
+  //console.log(body)
   //Extraindo a propriedade results do body
-  const results = body.results;
+  const results = body.results
+
+  console.log (results)
+  //Atualizar o estado "itens" da API
+
+  setItens(results)
   }
 
   carregarDadosApi ()
